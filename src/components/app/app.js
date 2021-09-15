b2f5058b7df7337fee4f7261af693f79ae226a31
Павел <pavel.tagiltsef@gmail.com>
2021-09-15 19:46:43 +0300

    Добавляет возможность фильтрации постов

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,12 +17,14 @@ export default class App extends Component {
                 {label: 'Going to learn React', important: true, like: false, id: 'ejsalgj;ldagif'},
                 {label: 'That is so good', important: false, like: false, id: 'isafjidsafldks'},
                 {label: 'I need a breake...', important: false, like: false, id: 'dlksajfdisalif'}
-            ]
+            ],
+            term: ''
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
     }
 
     deleteItem(id) {
@@ -83,10 +85,27 @@ export default class App extends Component {
         })
     }
 
+    searchPost(items, term) {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter((item) => {
+            return item.label.indexOf(term) > -1
+        });
+    }
+
+    onUpdateSearch(term) {
+        this.setState({term});
+    }
+
     render() {
-        const {data} = this.state;
+        const {data, term} = this.state;
+
         const liked = data.filter((item) => item.like).length;
         const allPosts = data.length;
+
+        const visiblePosts = this.searchPost(data, term);
 
         return (
             <div className="app">
@@ -94,11 +113,12 @@ export default class App extends Component {
                     liked={liked}
                     allPosts={allPosts}/>
                <div className="search-panel d-flex">
-                    <SearchPanel/>
+                    <SearchPanel
+                    onUpdateSearch={this.onUpdateSearch}/>
                     <PostStatusFilter/>
                </div>
                 <PostList 
-                    posts={this.state.data}
+                    posts={visiblePosts}
                     onDelete={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleLiked={this.onToggleLiked}/>
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,29 @@
+import { Component } from 'react';
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return (
-        <input
-            className="form-control search-input"
-            type="text"
-            placeholder="Поиск по записям"
-        />
-    )
-};
+export default class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        };
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    }
 
-export default SearchPanel;
+    onUpdateSearch(evt) {
+        const term = evt.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+
+    render() {
+        return (
+            <input
+                className="form-control search-input"
+                type="text"
+                placeholder="Поиск по записям"
+                onChange={this.onUpdateSearch}
+            />
+        )
+    }
+};
